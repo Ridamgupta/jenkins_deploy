12ad9f7f6da47dbf72273dc5779e4e12f8b835b9
Ridam Gupta <104208414+Ridamgupta@users.noreply.github.com>
2024-04-02 10:14:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def hello():
     X, y = iris.data, iris.target
     model = RandomForestClassifier()
     model.fit(X, y)
+    print("hi")
     return jsonify({'message': 'Model trained successfully!(hello)'})
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
